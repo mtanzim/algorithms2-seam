f2f3ee3f124bec4a025ebba11e6128522afe894f
Tanzim Mokammel <mtanzim@gmail.com>
2019-09-10 18:57:02 +0200

    start

---
+++ SeamCarver.java
@@ -0,0 +1,63 @@
+/* *****************************************************************************
+ *  Name: SeamCarver
+ *  Date: Sep 10, 2019
+ *  Description: Entry for Seam
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.Picture;
+import edu.princeton.cs.algs4.StdOut;
+
+public class SeamCarver {
+    Picture picture;
+
+    // create a seam carver object based on the given picture
+    public SeamCarver(Picture picture) {
+        this.picture = picture;
+
+    }
+
+    // current picture
+    public Picture picture() {
+        return picture;
+    }
+
+    // width of current picture
+    public int width() {
+        return picture.width();
+    }
+
+    // height of current picture
+    public int height() {
+        return picture.height();
+    }
+
+    // energy of pixel at column x and row y
+    public double energy(int x, int y) {
+        return 0.0;
+    }
+
+    // sequence of indices for horizontal seam
+    public int[] findHorizontalSeam() {
+        return new int[] { 0, 0 };
+    }
+
+    // sequence of indices for vertical seam
+    public int[] findVerticalSeam() {
+        return new int[] { 0, 0 };
+
+    }
+
+    // remove horizontal seam from current picture
+    public void removeHorizontalSeam(int[] seam) {
+
+    }
+
+    public void removeVerticalSeam(int[] seam) {
+
+    }
+
+    // remove vertical seam from current picture
+    public static void main(String[] args) {
+        StdOut.println("Hello from Seam.");
+    }
+}
